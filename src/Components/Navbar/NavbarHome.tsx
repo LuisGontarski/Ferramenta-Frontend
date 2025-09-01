afa29fce9ea711877aa649213fd9e5850f72acfa
Gustavo dos Santos Capeletti <gustavocapeletti007@gmail.com>
2025-09-01 00:11:18 -0300

    Ajustes design

--- src/Components/Navbar/NavbarHome.tsx
+++ src/Components/Navbar/NavbarHome.tsx
@@ -1,5 +1,8 @@
 import "./NavbarHome.css";
 import { Link } from "react-router-dom";
+import { IoDocumentTextOutline } from "react-icons/io5";
+import { LuChartColumn, LuCalendar } from "react-icons/lu";
+import { IoMdCheckboxOutline } from "react-icons/io";
 
 const NavbarHome = () => {
   return (
@@ -10,10 +13,10 @@ const NavbarHome = () => {
 
         <nav className="navbarHome">
           <div className="div-menu">
-            <a href="/kanban">Kanban</a>
-            <a href="/cronograma">Cronograma</a>
-            <a href="/documentos">Documentos</a>
-            <a href="/requisitos">Requisitos</a>
+            <a href="/kanban"><LuChartColumn /> <span>Kanban</span></a>
+            <a href="/cronograma"><LuCalendar /> <span>Cronograma</span></a>
+            <a href="/documentos"><IoDocumentTextOutline /> <span>Documentos</span></a>
+            <a href="/requisitos"><IoMdCheckboxOutline /> <span>Requisitos</span></a>
           </div>
 
           <div className="div-other">
