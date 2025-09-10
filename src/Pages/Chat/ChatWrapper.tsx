e8ee0cc4fb23994567bf4810e01212c5a5c37ecd
LuisGontarski <luisedgon@outlook.com>
2025-09-10 00:45:49 -0300

    att

--- src/Pages/Chat/ChatWrapper.tsx
+++ src/Pages/Chat/ChatWrapper.tsx
@@ -1,13 +1,15 @@
 import { useParams } from "react-router-dom";
 import Chat from "./Chat";
 
+
+
 const ChatWrapper = () => {
   // Pega o projeto_id da rota, ex: /chat/123e4567-e89b-12d3-a456-426614174000
   const { projeto_id } = useParams<{ projeto_id: string }>();
 
   // Aqui você pode pegar o usuário logado do seu contexto/auth
   const usuario_id = "89017ccf-58ee-4c91-95f6-17c9ca80f00f"; // substituir pelo ID real do usuário logado
-  const usuario_nome = "Gustavo"; // substituir pelo nome real do usuário logado
+  const usuario_nome = "LuisPO"; // substituir pelo nome real do usuário logado
 
   if (!projeto_id) {
     return <div>Projeto não definido</div>;
